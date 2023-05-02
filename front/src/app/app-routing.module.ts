b940de974863463d8e458673b4aadc60a1663b95
MTX Devp <MTXDevp@hotmail.com>
2023-05-02 23:03:51 +0200

    add random components

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeaderComponent } from './components/componente1/header.component';
+import { MainComponent } from './components/componente2/main.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+
+  { path: 'header', component: HeaderComponent },
+  { path: 'main', component: MainComponent },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './components/componente2/main.component';
+import { HeaderComponent } from './components/componente1/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MainComponent,
   ],
   imports: [
     BrowserModule,
